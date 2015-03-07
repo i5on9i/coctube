c6bc89493fb5bd248f7d0e5f6da7d8c2123cca6c
i5on9i <gaedduck@gmail.com>
2015-03-07 10:26:11 +0900

    - add two more channels on coc2 fragment

---
+++ app/src/main/java/com/cocube/parser/coctube2/CocCamParserInfo.java
@@ -0,0 +1,20 @@
+package com.cocube.parser.coctube2;
+
+
+import com.cocube.parser.ChannelParserInfo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: namh
+ * Date: 13. 9. 14
+ * Time: 오전 12:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class CocCamParserInfo extends ChannelParserInfo {
+
+    private static final String CHANNEL_ID = "UCT2x1vuvgYdhk-kQdlzn6yA";
+
+    public CocCamParserInfo() {
+        super(CHANNEL_ID);
+    }
+}
---
+++ app/src/main/java/com/cocube/parser/coctube2/HaVocGamingParserInfo.java
@@ -0,0 +1,20 @@
+package com.cocube.parser.coctube2;
+
+
+import com.cocube.parser.ChannelParserInfo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: namh
+ * Date: 13. 9. 14
+ * Time: 오전 12:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class HaVocGamingParserInfo extends ChannelParserInfo {
+
+    private static final String CHANNEL_ID = "UC99fa54IUf9RVpEHaUTxQ9w";
+
+    public HaVocGamingParserInfo() {
+        super(CHANNEL_ID);
+    }
+}
